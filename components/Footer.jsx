e4e0f4d967937624373c947962e56ee6eb9aa2fc
Walid Bouladam <bouladam.walid86@gmail.com>
2022-07-13 10:51:13 +1200

    Created Footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,15 @@
+import { Box } from '@chakra-ui/react'
+
+const Footer = () => (
+  <Box
+    textAligh="center"
+    p="5"
+    color="gray.600"
+    borderTop="1px"
+    borderColor="gray.100"
+  >
+    Cloud9 Realtor, Inc. &copy; {new Date().getFullYear()}
+  </Box>
+)
+
+export default Footer
